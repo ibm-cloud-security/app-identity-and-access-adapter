5586f7147cb624a8fbd5a15f5995a6e8175e172a
Aaron Liberatore <aaron.m.liberatore@ibm.com>
2019-04-19 08:48:29 -0500

    Clean up

--- helm/ibmcloudappid/templates/_helpers.tpl
+++ helm/ibmcloudappid/templates/_helpers.tpl
@@ -11,7 +11,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "appid-istio-mixer-adapter.fullname" -}}
+{{- define "ibmcloudappid.fullname" -}}
 {{- if .Values.fullnameOverride -}}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
 {{- else -}}
@@ -27,6 +27,6 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "appid-istio-mixer-adapter.chart" -}}
+{{- define "ibmcloudappid.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
 {{- end -}}
--- monitor/appidconfig.go
+++ monitor/appidconfig.go
@@ -2,18 +2,12 @@ package monitor
 
 import (
 	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"time"
 
 	"istio.io/istio/pkg/log"
 )
 
 const (
-	appIDURL        = "APPID_URL"
-	appIDApiKey     = "APPID_APIKEY"
 	clusterName     = "CLUSTER_NAME"
 	clusterGUID     = "CLUSTER_GUID"
 	clusterType     = "CLUSTER_TYPE"
@@ -68,56 +62,11 @@ type ClientCredentials struct {
 func NewAppIDConfig() (*AppIDConfig, error) {
 	appidCfg := &AppIDConfig{}
 
-	// Retrieve Environment Variables
-	appidCfg.AppidURL = os.Getenv(appIDURL)
-	appidCfg.AppidAPIKey = os.Getenv(appIDApiKey)
-	clsterName := os.Getenv(clusterName)
-	clsterGUID := os.Getenv(clusterGUID)
-	clsterType := os.Getenv(clusterType)
-	clsterLocation := os.Getenv(clusterLocation)
-
-	log.Infof("Config")
-	log.Infof("APPID_URL: %s", appidCfg.AppidURL)
-	log.Infof("APPID_APIKEY: %s", appidCfg.AppidAPIKey)
-	log.Infof("CLUSTER_NAME: %s", clsterName)
-	log.Infof("CLUSTER_TYPE: %s", clsterType)
-	log.Infof("CLUSTER_LOCATION: %s", clsterLocation)
-	log.Infof("CLUSTER_GUID: %s", clsterGUID)
-
-	if appidCfg.AppidURL == "" || appidCfg.AppidAPIKey == "" || clsterName == "" || clsterGUID == "" || clsterLocation == "" || clsterType == "" {
-		log.Errorf("Missing one of the following environment variables: APPID_URL APPID_APIKEY CLUSTER_NAME CLUSTER_GUID CLUSTER_LOCATION CLUSTER_TYPE")
-		log.Error("Shutting down....")
-		return nil, errors.New("Missing one or more env variables")
-	}
-
 	appidCfg.Port = defaultPort
 	if len(os.Args) > 1 {
 		appidCfg.Port = os.Args[1]
 	}
-
-	// Retrieve App ID instance configuration
-	credentials, err := retrieveAppIDConfig(appidCfg.AppidURL, appidCfg.AppidAPIKey)
-	if err != nil {
-		return nil, errors.New("could not retrieve App ID instance credentials")
-	}
-
-	appidCfg.ClientCredentials = credentials
-	appidCfg.ClusterInfo = &ClusterInfo{
-		Name:     clsterName,
-		GUID:     clsterGUID,
-		Type:     clsterType,
-		Location: clsterLocation,
-		Services: make(map[string]Service),
-	}
-
-	res, _ := json.MarshalIndent(appidCfg, "", "\t")
-	log.Debugf("Retrieved configuration:\n %s", string(res))
-
-	return appidCfg, nil
-}
-
-func retrieveAppIDConfig(url string, apiKey string) (*ClientCredentials, error) {
-	return &ClientCredentials{
+	appidCfg.ClientCredentials = &ClientCredentials{
 		TenantID:         "798288dc-79cb-4faf-9825-dad68cd4ed6f",
 		ClientID:         "7a2a8cb8-774e-49e0-90c2-425f03aecec6",
 		Secret:           "Y2VjNGIwNjctOTEyMy00NTQ0LTg0NjgtZTJjYTA3MjNhYjFl",
@@ -125,48 +74,14 @@ func retrieveAppIDConfig(url string, apiKey string) (*ClientCredentials, error) 
 		TokenURL:         "https://appid-oauth.ng.bluemix.net/oauth/v3/798288dc-79cb-4faf-9825-dad68cd4ed6f/token",
 		UserinfoURL:      "https://appid-oauth.ng.bluemix.net/oauth/v3/798288dc-79cb-4faf-9825-dad68cd4ed6f/userinfo",
 		JwksURL:          "https://appid-oauth.ng.bluemix.net/oauth/v3/798288dc-79cb-4faf-9825-dad68cd4ed6f/publicKeys",
-	}, nil
-
-	log.Infof(">> retrieveAppIDConfig")
-	client := &http.Client{
-		Timeout: 5 * time.Second,
 	}
 
-	req, err := http.NewRequest("GET", url+"/config", nil)
-	req.Header.Add("X-Api-Key", apiKey)
-	if err != nil {
-		log.Errorf("<< retrieveAppIDConfig FAIL http.NewRequest :: %s", err)
-		return nil, err
+	appidCfg.ClusterInfo = &ClusterInfo{
+		Services: make(map[string]Service),
 	}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Errorf("<< retrieveAppIDConfig FAIL client.Do :: %s", err)
-		return nil, err
-	}
+	res, _ := json.MarshalIndent(appidCfg, "", "\t")
+	log.Debugf("Retrieved configuration:\n %s", string(res))
 
-	if resp.StatusCode == 403 {
-		log.Errorf("<< retrieveAppIDConfig 403")
-		return nil, errors.New("403 Forbidden")
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	log.Debugf("retrieveAppIDConfig Response body: %s", string(body))
-
-	if err != nil {
-		log.Errorf("<< retrieveAppIDConfig FAIL ioutil.ReadAll :: %s", err)
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	appidCreds := ClientCredentials{}
-	err = json.Unmarshal(body, &appidCreds)
-	if err != nil {
-		log.Errorf("<< retrieveAppIDConfig FAIL json.Unmarshal :: %s", err)
-		return nil, err
-	}
-	log.Debugf("<< retrieveAppIDConfig OK")
-
-	return &appidCreds, nil
+	return appidCfg, nil
 }
